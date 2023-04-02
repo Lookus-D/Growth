501d3fd3dde001920fd9aed51f11e6b2df787c4d
User <User@DESKTOP-ROOTJT0>
2023-04-02 22:41:09 +0200

    Add project files.

---
+++ Growth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Growth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me your height, please.");
+            string height = Console.ReadLine();
+            int chosenHeight = 0;
+            Int32.TryParse(height, out chosenHeight);
+
+            if ((chosenHeight >140) && (chosenHeight <150))
+            {
+                Console.WriteLine("You are really small, dwarf.");
+            }
+            else
+            {
+                Console.WriteLine("Wow, it's incredible!");
+            }
+        }
+    }
+
+}
